5bd75888e8630450c986b320de2421892919fc70
Maximiliano Gasquez <110992353+MaGadev69@users.noreply.github.com>
2025-07-04 00:35:09 -0300

    Update token.sol

--- token.sol
+++ token.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.8.27;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @title MyToken
+ * @author Gasquez_Jonatan
+ * @notice A basic ERC20 token with a minting function.
+ */
 contract MyToken is ERC20 {
     constructor(string memory sym, string memory name)
         ERC20(name, sym)
